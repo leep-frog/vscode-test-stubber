acbe503eed35fc3dd6416d3d0af514f8f1066dfe
Greg Leeper <gleeper49@gmail.com>
2024-05-02 15:03:44 -0400

    fix test coverage generation

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
-	files: 'dist/test/**/*.test.js',
+	files: 'out/test/**/*.test.js',
   mocha: {
     timeout: 60000,
   },
