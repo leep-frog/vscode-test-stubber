5405004f03cf941cd6d53fefa8b8cc398e80e6de
Greg Leeper <gleeper49@gmail.com>
2024-05-07 10:55:05 -0400

    Use local vscode version

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from '@vscode/test-cli';
+import path from 'path';
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+  files: 'out/test/**/*.test.js',
+  useInstallation: {
+    fromMachine: true,
+    // This needs to be a different version than the one I'm actually using, otherwise test doesn't run
+    fromPath: path.resolve(".vscode-test", "vscode-win32-x64-archive-1.86.0", "Code.exe"),
+  },
+  workspaceFolder: path.resolve("src", "test", "test-workspace"),
+  // env: {
+  //   VSCODE_STUBBABLE_TEST_FILE: stubbableTestFile,
+  // },
   mocha: {
     timeout: 60000,
+    // bail: true,
   },
 });
